92b20f6cda547c3d6a8088e8a73b7e37c5bef6d3
Shuchen Zhu <shuchenzhu@Hongkais-iMac.local>
2017-10-16 20:09:27 -0400

    display trained topics

---
+++ lda/display.py
@@ -0,0 +1,18 @@
+import logging
+
+from gensim.models import LdaModel
+from gensim import corpora
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+dictionary_path = "models/dictionary.dict"
+corpus_path = "models/corpus.lda-c"
+lda_num_topics = 75
+lda_model_path = "models/lda_model_50_topics.loggingda"
+
+dictionary = corpora.Dictionary.load(dictionary_path)
+corpus = corpora.BleiCorpus(corpus_path)
+lda = LdaModel.load(lda_model_path)
+
+for i, topic in enumerate(lda.show_topics(num_topics=lda_num_topics)):
+	print ('#%i: %s' %(i,str(topic)))
