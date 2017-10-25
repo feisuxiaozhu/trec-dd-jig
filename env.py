1fdc30742be4aab5f2aca298443276de276c07c7
Shuchen Zhu <shuchenzhu@Hongkais-iMac.local>
2017-10-24 23:17:50 -0400

    will work on build_vector_reserve() tomorrow, need the values of keys to be vector

--- env.py
+++ env.py
@@ -28,6 +28,10 @@ def make_query_dsl(s):
 
 	return query
 
+def make_query_mongo(s):
+
+	query = {"docId": s}
+	return query
 #read hiearchy from .txt file prepared by ../generate_hiearchy_rep.py, return a dictionary
 def read_hiearchy():
 	dic = {}
@@ -43,8 +47,7 @@ def read_hiearchy():
 			vector.pop(0) #get rid of [
 			vector.pop(-1) #get rid of ]
 			vector = list(map(float,vector)) #convert the list from string to float
-			dic[id]=vector
-			
+			dic[id]=vector		
 	return dic
 
 
@@ -53,8 +56,9 @@ class env:
 	def __init__(self, topic_name, topic_id):
 		self.topic_id = topic_id
 		self.topic_name = topic_name
-		self.reserve = self._build_reserve()
 		self.hiearchy_map = read_hiearchy()
+		self.reserve = self._build_reserve() #a dictionary of returned doc, key=docid, value=search score
+		self.reserve_vector = self.build_vector_reserve() #a dicionary of returned doc, key=docid, value=hiearchy vector
 
 	#hold first 500 returned document from given query = topic_name
 	#documents are represented by a docID and a topic vector
@@ -68,20 +72,32 @@ class env:
 		temp = raw_result['hits']['hits']
 		b=''
 		for a in temp:
-			b = b + str(a['_id']) + ":" + str(a['_score'])+" "
-		results[self.topic_id] = b 
+			results[a['_id']] = a['_score']
 
 		return results
 
 	#connect to MongoDB and add topic vector to each doc in reserve list
 	#def vector_addto_reserve(self):
+	def build_vector_reserve(self):
+		result = {}
+
+		lda_collection = MongoClient(MONGO_CONNECTION_STRING)[TAGS_DATABASE][LDA_COLLECTION]
+		for i, j in self.reserve.items():
+			a = make_query_mongo(i)
+			#make sure you indexex the mongoDB, otherwise so slow
+			lda_cursor = lda_collection.find_one(a)
+			result[i] = lda_cursor['lda']
+
+		return result
 
 
 
 
 
-a = env('fuck off','dd1')
-print (a.reserve)
+
+a = env('knock off','dd1')
+print(a.reserve_vector)
+
 
 
 
