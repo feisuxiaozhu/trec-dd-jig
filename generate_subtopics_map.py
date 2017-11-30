cb4e6a0d02b0601c7dcf967c1d289c2b60bbea44
Shuchen Zhu <shuchenzhu@Hongkais-iMac.local>
2017-11-29 21:53:53 -0500

    it read all the subtopics passages and save files in directory subtopics

--- generate_subtopics_map.py
+++ generate_subtopics_map.py
@@ -3,6 +3,7 @@ import json
 import os
 from config import *
 
+
 def parse_raw_queires(raw_data):
 	queires = []
 	raw_queries = re.split(r'</topic>', raw_data)
@@ -16,16 +17,29 @@ def parse_raw_queires(raw_data):
 			q_topicname = re.search(r'([\w\W]+)" id',raw).group(1)
 			q_topicname = re.sub('[/]','',q_topicname)
 			q_number_of_subtopics = re.search(r'num_of_subtopics="(\d+)"',raw_query).group(1)
-			
+			q_number = q_number[5:]
 			raw_subtopics = re.split(r'</subtopic>', raw_query)
-			for i in range(int(q_number_of_subtopics)):
-				print (i+1)
-
-		queires.append({'_number': q_number, 'topicname': q_topicname, 'num_of_subtopics': q_number_of_subtopics})
-	return queires
-
+			counter = 1
+			for raw_subtopic in raw_subtopics:
+				if counter > int(q_number_of_subtopics): break
+				extracted=''
+				subtopic_number = counter
+				passages = re.split(r'</passage>', raw_subtopic)
+				for passage in passages:
+					text = re.search(r'<text>([\s\w\W]+)</text>', passage)
+					if bool(text)==True:
+						text=text.group(1)
+						text=text[9:-3]
+						extracted = extracted + text + '\n'
+				filename=SUBTOPIC_FP
+				
+				subtopic_number = str(counter)
+				filename=filename+'/'+q_number+'/'+subtopic_number+'.txt'
+				os.makedirs(os.path.dirname(filename), exist_ok=True)
+				with open(filename, "w") as f:
+					f.write(extracted)
+				counter += 1
+				
 
 with open(QUERIES_FP) as f:
 	queires = parse_raw_queires(f.read())
-
-print(queires)
