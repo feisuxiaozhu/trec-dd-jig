32c432c7395c54d03dccebc978773dfcaa16cf03
Shuchen Zhu <shuchenzhu@Hongkais-iMac.local>
2018-01-21 22:46:09 -0500

    m

---
+++ main_train_random.py
@@ -0,0 +1,114 @@
+from env import environment
+from dqn import DQNAgent
+from config import *
+import numpy as np
+import os
+from config import *
+import re
+import json
+import random
+
+with open(TOPIC_FP) as f:
+	topics = f.readlines()
+
+#first create an agent for training
+state_size = 75
+action_size = 12
+agent = DQNAgent(state_size, action_size)
+print("DQN agent built")
+done = False
+batch_size = 32
+
+#create an environment for training on different topics
+EPISODES = 30
+RESERVE= 300
+ITERATION = 10
+
+
+reward_2 = 0
+reward_3 = 0
+reward_4 = 0
+reward_5 = 0
+reward_6 = 0
+reward_7 = 0
+reward_8 = 0
+
+#Now run the trained agent and see how it works:
+N = 10 #total of ten rounds
+results={}
+for i in topics:
+	reward_record = []
+	topic_number = i.split()[0]
+	topic_id = 'dd17-'+str(topic_number)
+	topic_name_tokens = i.split()[1:]
+	topic_name =''
+	for j in topic_name_tokens:
+		topic_name = topic_name + j +' '
+		topic_name = re.sub('[/]','',topic_name)
+	env = environment(topic_name,topic_id,state_size,ITERATION, RESERVE)
+	os.system('clear')
+	print('testing agent on topic: '+ topic_id+' '+topic_name)
+	#start 10 interactions between search engine and user
+	state = env.state
+	state = np.reshape(state,[1,state_size])
+	#state = state / env.num_of_on_topics
+	for k in range(N):
+		action = random.randint(1,12)
+		action = str(action)
+		next_state, reward, done = env.step(action)
+		if k == 0: reward_2 += reward
+		if k == 1: reward_3 += reward
+		if k == 2: reward_4 += reward
+		if k == 3: reward_5 += reward
+		if k == 4: reward_6 += reward
+		if k == 5: reward_7 += reward
+		if k == 6: reward_8 += reward
+
+		reward_record.append(reward)
+		if done:
+			break
+		#if reward_record[k-1]/reward_record[k]>=2: #huge drop of number of on topic docs
+		 	#break
+		next_state = np.reshape(next_state, [1,state_size])
+		#next_state = next_state / env.num_of_on_topics
+		action = int(action)-1
+		state = next_state
+	#after 10 rounds of interaction, save the search history into results for each topic
+	results[topic_id] = env.search_history
+
+#save the trained search result to .txt file:
+json.dump(results, open('trained_search_doc.txt','w'))
+print('search results has been saved in file trained_search_doc.txt')
+
+
+reward_2 = reward_2/60
+reward_3 = reward_3/60
+reward_4 = reward_4/60
+reward_5 = reward_5/60
+reward_6 = reward_6/60
+reward_7 = reward_7/60
+reward_8 = reward_8/60
+
+print('average reward 2: ' +str(reward_2))
+print('average reward 3: ' +str(reward_3))
+print('average reward 4: ' +str(reward_4))
+print('average reward 5: '+ str(reward_5))
+print('average reward 6: ' +str(reward_6))
+print('average reward 7: ' +str(reward_7))
+print('average reward 8: ' +str(reward_8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
